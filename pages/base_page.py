561f08c1237a14521f656d5163bdb6fddcc8f7b8
Kate Vauchok <kate.makar2024@gmail.com>
2025-02-07 10:59:39 +0100

    Тест коммит

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
+# hjjdhjfd
 class BasePage():
     def __init__(self, driver):
         self.driver = driver
